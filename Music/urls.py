4412a5e44a0e645c406d7d425e0add1185a2eb2a
FlyingFish-233 <loftythinking@126.com>
2025-06-28 00:56:32 +0800

    网页demo

--- Music/urls.py
+++ Music/urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 import Music.views as views
 
 urlpatterns = [
+    path('', views.song_list, {'id': 1}, name='root'),
+    path('song', views.song_list, {'id': 1}, name='song'),
+    path('song/<int:id>', views.song_list, name='song_list'),
 ]
--- Music/views.py
+++ Music/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from Music.models import Singer, Song, Comment
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
+LIST_NUM = 20
+
+def song_list(request, id):
+    song_start = (id - 1) * LIST_NUM
+    song_end = min(id * LIST_NUM - 1, Song.objects.count() - 1)
+    if song_start >= Song.objects.count():
+        return HttpResponseRedirect('/song')
+    songs = Song.objects.filter(id__range=(song_start, song_end))
+    
+    template = loader.get_template("song/list.html")
+    context = {
+        'songs' : songs,
+    }
+    return HttpResponse(template.render(context, request))
